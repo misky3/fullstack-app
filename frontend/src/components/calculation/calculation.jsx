50cd873277e0b1bea1afa9583927c8f61a5c3950
misky3 <33999758+misky3@users.noreply.github.com>
2025-08-15 18:09:58 +0800

    done

--- frontend/src/components/calculation/calculation.jsx
+++ frontend/src/components/calculation/calculation.jsx
@@ -5,23 +5,25 @@ import { useState, useEffect } from 'react';
 function Calculation({userId, income, totalExpenses, onReset}){
     return(
         <div className='calculation'>
-            <div style={{color:"black"}}>Calculation</div>
+            <div className = 'calcu'>Calculation</div>
             <div style={{border:'1px dashed black', width: '310px'}}></div>
-            <div>
+            <div className='income'>
                 <div style={{color:"black"}}>Income</div>
-                <div style={{color:"black"}}>{income}</div>
+                <div style={{color:"black", fontSize:"30px"}}>P{income}</div>
             </div>
             <DoughnutChart income={income} totalExpenses={totalExpenses}/>
-            <div>
-                <div style={{color:"black"}}>Available</div>
-                <div style={{color:"black"}}>${income - totalExpenses}</div>
-            </div>
-            <div>
-                <div style={{color:"black"}}>Spent</div>
-                <div style={{color:"black"}}>${totalExpenses}</div>
+            <div className='numbers'>
+                <div className='box'>
+                    <div style={{color:"white", fontSize: "13px"}}>Available</div>
+                    <div style={{color:"green", fontSize: "20px"}}>P{income - totalExpenses}</div>
+                </div>
+                <div className='box'>
+                    <div style={{color:"white", fontSize: "13px"}}>Spent</div>
+                    <div style={{color:"yellow", fontSize: "20px"}}>P{totalExpenses}</div>
+                </div>
             </div>
             <div style={{border:'1px dashed black', width: '310px'}}></div>
-            <button onClick={onReset}>Reset Expense</button>
+            <button onClick={onReset} className='reset'>Reset Expense</button>
         </div>
     );
 }
